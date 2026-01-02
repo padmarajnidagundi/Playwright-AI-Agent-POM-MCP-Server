ce8e53e177959a6602bdabb0d96889e85b89aef4
Padmaraj Nidagundi <padmaraj.nidagundi@gmail.com>
2026-01-02 20:31:19 +0200

    origin/main

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    // Add custom rules here if needed
+  },
+};
